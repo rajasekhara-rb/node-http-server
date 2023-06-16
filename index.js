e99d53ee3be7855d4130ef3b54a6cbb84847ef7d
Budda Rajasekhara Reddy <rajasekharabudda009@gmail.com>
2023-06-16 22:44:37 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+// imprting the http 
 const http = require('http');
 
+// define port 
 const port = 9999;
 
-http.createServer((req, resp) => {
+// create server 
+const server = http.createServer((req, resp) => {
+    // resp.statusCode=200;
+    // resp.setHeader("Content-Type", "text/html" )
     resp.writeHead(200, { "Content-Type": "text/html" })
     resp.write("Hello the server is started");
-    console.log("server started at " + port)
     resp.end();
-}).listen(9999);
+})
+
+server.listen(port, () => {
+    console.log("server started at " + port)
+});
 
